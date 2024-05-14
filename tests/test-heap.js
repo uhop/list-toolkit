5cd9c5816ea708d3853d8780fe0cdbde79e412cb
Eugene Lazutkin <eugene.lazutkin@gmail.com>
2024-05-14 12:51:25 -0500

    Better tests.

--- tests/test-heap.js
+++ tests/test-heap.js
@@ -17,24 +17,32 @@ test('Heap', t => {
   t.notOk(heap.isEmpty);
   t.equal(heap.length, 5);
 
-  t.equal(heap.top, 5);
-  t.equal(heap.pop(), 5);
-  t.equal(heap.top, 4);
-  t.equal(heap.pop(), 4);
-  t.equal(heap.top, 3);
-  t.equal(heap.pop(), 3);
-  t.equal(heap.top, 2);
-  t.equal(heap.pop(), 2);
-  t.equal(heap.top, 1);
-  t.equal(heap.pop(), 1);
+  {
+    const tops = [],
+      pops = [];
+    while (!heap.isEmpty) {
+      tops.push(heap.top);
+      pops.push(heap.pop());
+    }
+    t.deepEqual(tops, [5, 4, 3, 2, 1]);
+    t.deepEqual(pops, [5, 4, 3, 2, 1]);
+  }
   t.equal(heap.top, undefined);
   t.equal(heap.pop(), undefined);
 
   t.ok(heap.isEmpty);
 
-  heap.push(3).push(2).push(5).push(1).push(4);
-  const array = heap.releaseSorted();
-  t.deepEqual(array, [1, 2, 3, 4, 5]);
+  {
+    const heap = new Heap((a, b) => a < b, [3, 2, 5, 1, 4]);
+    const array = heap.releaseSorted();
+    t.deepEqual(array, [1, 2, 3, 4, 5]);
+  }
+
+  {
+    const heap = new Heap().push(3).push(2).push(5).push(1).push(4);
+    const array = heap.releaseSorted();
+    t.deepEqual(array, [1, 2, 3, 4, 5]);
+  }
 
   heap.clear();
   t.ok(heap.isEmpty);
