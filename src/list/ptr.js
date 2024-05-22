424f62c5ca566b9dd353d4fefd0b17560da4351a
Eugene Lazutkin <eugene.lazutkin@gmail.com>
2024-05-22 11:28:34 -0500

    Made methods `addXXX()` and `insertXXX()` return a pointer.

--- src/list/ptr.js
+++ src/list/ptr.js
@@ -3,7 +3,7 @@
 import {HeadNode, PtrBase} from './nodes.js';
 import {pop, splice} from './basics.js';
 
-export class Ptr  extends PtrBase {
+export class Ptr extends PtrBase {
   constructor(list, node) {
     super(list, node, HeadNode);
     this.node ||= this.list.front;
@@ -21,26 +21,28 @@ export class Ptr  extends PtrBase {
     return pop(this.list, node).extracted;
   }
   addBefore(value) {
-    splice(this.list, this.node, this.list.adoptNode(value));
-    return this;
+    const node = this.list.adoptNode(value);
+    splice(this.list, this.node, node);
+    return this.list.makePtr(node);
   }
   addAfter(value) {
-    splice(this.list, this.node[this.list.nextName], this.list.adoptNode(value));
-    return this;
+    const node = this.list.adoptNode(value);
+    splice(this.list, this.node[this.list.nextName], node);
+    return this.list.makePtr(node);
   }
   insertBefore(list) {
     if (!this.list.isCompatible(list)) throw new Error('Incompatible lists');
-    if (list.isEmpty) return this;
+    if (list.isEmpty) return null;
     const head = pop(list, list).rest;
     splice(this.list, this.node, head);
-    return this;
+    return this.list.makePtr(head);
   }
   insertAfter(list) {
     if (!this.list.isCompatible(list)) throw new Error('Incompatible lists');
-    if (list.isEmpty) return this;
+    if (list.isEmpty) return null;
     const head = pop(list, list).rest;
     splice(this.list, this.node[this.list.nextName], head);
-    return this;
+    return this.list.makePtr(head);
   }
 }
 
