8e1f97d6b6fe48dd552e60b8c2257caf268a3986
Eugene Lazutkin <eugene.lazutkin@gmail.com>
2024-06-02 23:53:40 -0500

    Better performing implementation.

--- src/cache/cache-random.js
+++ src/cache/cache-random.js
@@ -1,23 +1,72 @@
 'use strict';
 
+import {addAlias} from '../meta-utils.js';
+import MinHeap from '../min-heap.js';
 import CacheLRU from './cache-lru.js';
 
-// Evicts randomly.
+// Evicts on the first-in-first-out basis.
 
 export class CacheRandom extends CacheLRU {
+  constructor(capacity = 10) {
+    super(capacity);
+    this.heap = new MinHeap({less: (a, b) => a.value.id > b.value.id});
+    this.nextId = 0;
+  }
   use(key) {
     return this.dict.get(key);
   }
-  evictAndReplace(key, value) {
-    const node = this.list.front,
-      nextName = this.list.nextName,
-      n = Math.floor(Math.random() * this.dict.size);
-    for (let i = 0; i < n; ++i) node = node[nextName];
-    this.dict.delete(node.value.key);
+  update(node, value) {
+    node.value.value = value;
+    return this;
+  }
+  addNew(key, value) {
+    this.list.pushFront({key, value, id: this.nextId++});
+    const node = this.list.front;
     this.dict.set(key, node);
-    node.value = {key, value};
+    this.heap.push(node);
     return node;
   }
+  evictAndReplace(key, value) {
+    const index = Math.floor(this.heap.length * Math.random());
+
+    const node = this.heap.array[index],
+      isDecreased = value > node.value.value;
+
+    this.dict.delete(node.value.key);
+    this.dict.set(key, node);
+
+    node.value.key = key;
+    node.value.value = value;
+
+    this.heap.updateByIndex(index, isDecreased);
+
+    return node;
+  }
+  remove(key) {
+    const node = this.dict.get(key);
+    if (node) {
+      this.dict.delete(key);
+      this.list.removeNode(node);
+      this.heap.remove(node);
+    }
+    return this;
+  }
+  clear() {
+    super.clear();
+    this.heap.clear();
+    return this;
+  }
+  resetIndices() {
+    this.nextId = 0;
+    for (const item of this.heap) {
+      item.id = this.nextId++;
+    }
+    const array = this.heap.array;
+    this.heap.clear().merge(array);
+    return this;
+  }
 }
+
+addAlias(CacheRandom, 'remove', 'delete');
 
 export default CacheRandom;
