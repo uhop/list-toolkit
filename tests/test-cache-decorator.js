834792fb94df837b63c00630252522b7bc8ff856
Eugene Lazutkin <eugene.lazutkin@gmail.com>
2024-06-10 23:17:26 -0500

    More tests for cache decorators.

--- tests/test-cache-decorator.js
+++ tests/test-cache-decorator.js
@@ -4,7 +4,7 @@ import test from 'tape-six';
 import {decorate, decorateMethod, getCache} from 'list-toolkit/cache/decorator.js';
 import Cache from 'list-toolkit/cache/cache-lru.js';
 
-test('Cache decorators', t => {
+test('Cache decorateMethod()', t => {
 
   class Sample {
     static counter = 0;
@@ -36,4 +36,41 @@ test('Cache decorators', t => {
   t.equal(sample.m(2), 4);
 
   t.equal(getCache(sample, 'm').size, 3);
+  t.equal(sample.m.cache.size, 3);
+});
+
+test('Cache decorate()', t => {
+
+  class Sample {
+    static counter = 0;
+
+    m(value) {
+      return Sample.counter++;
+    }
+  }
+  decorate(Sample.prototype, 'm', new Cache(3));
+
+  const sample = new Sample();
+
+  t.equal(sample.m(1), 0);
+  t.equal(sample.m(1), 0);
+
+  t.equal(sample.m(2), 1);
+  t.equal(sample.m(2), 1);
+
+  t.equal(sample.m(3), 2);
+  t.equal(sample.m(3), 2);
+
+  t.equal(sample.m(1), 0);
+
+  t.equal(sample.m(4), 3);
+  t.equal(sample.m(4), 3);
+  t.equal(sample.m(1), 0);
+  t.equal(sample.m(3), 2);
+
+  t.equal(sample.m(2), 4);
+
+  t.throws(() => getCache(sample, 'm'));
+  t.equal(getCache(Sample.prototype, 'm').size, 3);
+  t.equal(sample.m.cache.size, 3);
 });
